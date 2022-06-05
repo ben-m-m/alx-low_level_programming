1f970770b5b34a4a1c345e6b57cd7f1534e7e2af
ben-m-m <mainaben23@gmail.com>
2022-06-05 01:09:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -c $CFILE
+#include <stdio.h>                                                                                                                                                                            
+/**                                                                                                                                                                                           
+ *prints a given string                                                                                                                                                                       
+ *results to 0                                                                                                                                                                                
+ */                                                                                                                                                                                           
+int main(void)                                                                                                                                                                                
+{                                                                                                                                                                                             
+  puts("Programming is like building a multilingual puzzle");                                                                                                                                 
+    return (0);                                                                                                                                                                               
+}
