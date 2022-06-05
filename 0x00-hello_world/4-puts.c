876b4217e43957e2b141ab9cc2342991107dc27c
ben-m-m <mainaben23@gmail.com>
2022-06-05 02:52:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                                                                                                                            
-/**                                                                                                                                                                                           
- *prints a given string                                                                                                                                                                       
- *results to 0                                                                                                                                                                                
- */                                                                                                                                                                                           
-int main(void)                                                                                                                                                                                
-{                                                                                                                                                                                             
-  puts("\"Programming is like building a multilingual puzzle");                                                                                                                                 
-    return (0);                                                                                                                                                                               
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
