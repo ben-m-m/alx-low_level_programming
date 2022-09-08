646f1def9411b7aa44126549f59e45483a60a6e7
ben-m-m <mainaben23@gmail.com>
2022-09-08 11:26:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * return: always (0)
+ *
+ */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
