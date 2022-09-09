305b4367560c17ab9d020dffd9bca51e99547803
ben-m-m <mainaben23@gmail.com>
2022-09-09 09:33:28 -0700

    print alphabet c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
