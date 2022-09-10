3c096f2b505eb6fdd1e6cbaeb481a8d88c9980ff
ben-m-m <mainaben23@gmail.com>
2022-09-10 10:13:04 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int a = "z"
+
+	for (a = "z"; a >= "a"; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
