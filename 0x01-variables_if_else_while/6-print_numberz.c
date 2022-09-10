4f2e786fe88912546e96373bb426de5f045292f8
ben-m-m <mainaben23@gmail.com>
2022-09-10 09:54:54 -0700

    putchar base 10 nums

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - base (10) single digits
+ *
+ * Return: Always (0)
+ *
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	for (c = 0; c < 10; c++)
+	{
+		puchar(c);
+	}
+	return (0);
+}
