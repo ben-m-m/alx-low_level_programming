02afdc3e8bd60f5a9ac070e1ba9eb5c336dd5406
ben-m-m <mainaben23@gmail.com>
2022-09-11 09:56:37 -0700

    3 digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - three digit combination
+ *
+ * Return: Always  (0)
+ */
+
+int main(void)
+{
+	int hunds;
+	int tens;
+	int ones;
+
+	for (hunds = 0; hunds <= 9; hunds++)
+	{
+	for (tens = hund + 1; tens <= 9; tens++)
+	{
+	for (ones = tens + 1; ones <= 9; ones++)
+	{
+	putchar(hund + '0');
+	putchar(tens + '0');
+	putchar(ones + '0');
+
+	if (hund < 7)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
