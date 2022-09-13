f9fe8dc1a7708c345c90142d7d969f8283372634
ben-m-m <mainaben23@gmail.com>
2022-09-13 09:25:55 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - (10) alphabet lines
+ *
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int alph;
+	int num;
+
+	for (num = 1; num <= 10; num++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+	}
+	_putchar('\n');
+}
