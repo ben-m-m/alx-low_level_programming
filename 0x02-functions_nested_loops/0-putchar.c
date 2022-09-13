fbad7615ea6f5b728d33d55cefa112e68f5102ee
ben-m-m <mainaben23@gmail.com>
2022-09-13 01:32:20 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *
+ * return (0)
+ */
+
+int main(void)
+{
+	char c[8] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(c[8]);
+	}
+	_putchar('\n');
+	return (0);
+}
