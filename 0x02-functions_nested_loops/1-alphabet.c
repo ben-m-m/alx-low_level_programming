ec8ccbbdd2a91ddbac8cc1bc51d318b8677231da
ben-m-m <mainaben23@gmail.com>
2022-09-13 05:26:50 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = "a"; a <= "z"; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+	return (0);
+}
