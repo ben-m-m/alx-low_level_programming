6ec96a1b2e073a12ad68ac02e4727363ba0ff8af
ben-m-m <mainaben23@gmail.com>
2022-09-15 10:13:10 -0700

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
