47cee00adbbb7fcf2bcb2491fd20c7b9f04151d4
ben-m-m <mainaben23@gmail.com>
2022-09-15 10:40:56 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14
+ *
+ * Return - Always (0)
+ */
+
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c >= 10)
+			{
+				_putchar((c / 10) + 48);
+			}
+			_putchar((c % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
