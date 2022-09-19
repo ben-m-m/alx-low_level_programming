d9eed7bff651f718c257ebd40efb3b178d9dd7f0
ben-m-m <mainaben23@gmail.com>
2022-09-19 09:49:08 -0700

    print string and new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - print string to stdout
  * @str: string parameter
@@ -16,6 +17,7 @@ void _puts(char *str)
 			putchar('\n');
 			break;
 		}
+
 		putchar(str[count]);
 		count++;
 	}
