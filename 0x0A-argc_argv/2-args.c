20fb8974d7c3735b66d8495ec51cbaeb61aa118b
ben-m-m <mainaben23@gmail.com>
2022-09-30 08:25:08 -0700

    print all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all arguments passed
+ * @argc: int
+ * @argv: array
+ * Return: (0)
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (argc >= 0)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
