7cf03fd01249bddb97b046c5a5175f7de8e70301
ben-m-m <mainaben23@gmail.com>
2022-10-05 09:42:28 -0700

    free memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees memory
+ * @grid: pointer to  grid int
+ * @height: int
+ * Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || grid == 0)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
