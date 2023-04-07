152ed43e9b0956b3d82486e60736d9c5c768e3e8
ben-m-m <mainaben23@gmail.com>
2023-04-07 12:37:12 +0000

    gives you the index of a key.,

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,9 +6,11 @@
  * @size: size of the array of the hash table
  *
  * Return: index of the key
+ * Description: Uses the djb2 algorithm
+ *
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return(hash_djb2(size) % key);
+	return (hash_djb2(size) % key);
 }
