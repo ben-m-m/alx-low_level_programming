caa02ebde66669a5deb1add6d9b7eee5ca98e56b
ben-m-m <mainaben23@gmail.com>
2023-07-13 11:17:16 +0300

    jump search algo kernel compliant

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value){
 	prev = index = 0;
 
 	do {
-		printk(KERN_INFO "Value checked array[%d] = [%d]\n", index, array[index]);
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
 		if (array[index] == value)
 			return (index);
@@ -30,11 +30,11 @@ int jump_search(int *array, size_t size, int value){
 		index = k * m;
 	} while (index < (int)size && array[index] < value);
 
-	printk(KERN_INFO "Value found between indexes [%d] and [%d]\n", prev, index);
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
 
 	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printk(KERN_INFO "Value checked array[%d] = [%d]\n", prev, array[prev]);
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)
 			return (prev);
 	}
